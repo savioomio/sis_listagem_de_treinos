7f60b0a67eba0419c61620ffce7fc9249563c432
Sávio Pessôa Afonso <115905335+savioomio@users.noreply.github.com>
2023-08-24 00:05:34 -0300

    Update settings.py

--- lis_tre_Sávio/settings.py
+++ lis_tre_Sávio/settings.py
@@ -104,6 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
+ALLOWED_HOSTS = ['sislistagemdetreinos-production.up.railway.app']
 
 LANGUAGE_CODE = 'en-us'
 
