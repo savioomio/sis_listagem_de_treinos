1b0984209d73952457e22b08b04b8305a9cb1b32
Sávio Pessôa Afonso <115905335+savioomio@users.noreply.github.com>
2023-08-27 20:32:29 -0300

    Update forms.py

--- lis_savio/forms.py
+++ lis_savio/forms.py
@@ -1,8 +1,14 @@
+# Importando o módulo forms do Django
 from django import forms
+# Importando o modelo Treino definido em models.py
 from .models import Treino
 
+# Definindo uma classe de formulário chamada TreinoForm
 class TreinoForm(forms.ModelForm):
+    # Classe Meta é usada para configurar metadados do formulário
     class Meta:
+        # Especificando o modelo associado a este formulário (Treino)
         model = Treino
+        # Especificando quais campos do modelo serão incluídos no formulário
         fields = ['nome', 'categoria', 'imagem']
 
