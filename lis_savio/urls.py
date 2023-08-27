5b5b0a0a0b1eabdd485e587e2ab43ed820c636a6
Sávio Pessôa Afonso <115905335+savioomio@users.noreply.github.com>
2023-08-27 20:33:12 -0300

    Update urls.py

--- lis_savio/urls.py
+++ lis_savio/urls.py
@@ -1,10 +1,15 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+# Importa as views definidas no mesmo diretório
 from . import views
 
+# Lista de URLs do aplicativo
 urlpatterns = [
+    # Mapeia a URL raiz para a função 'lista_treinos' na view, nomeando-a como 'lista_treinos'
     path('', views.lista_treinos, name='lista_treinos'),
+    # Mapeia a URL 'adicionar/' para a função 'adicionar_treino' na view, nomeando-a como 'adicionar_treino'
     path('adicionar/', views.adicionar_treino, name='adicionar_treino'),
+    # Mapeia a URL 'editar/<int:pk>/' para a função 'editar_treino' na view, nomeando-a como 'editar_treino'
     path('editar/<int:pk>/', views.editar_treino, name='editar_treino'),
+    # Mapeia a URL 'excluir/<int:pk>/' para a função 'excluir_treino' na view, nomeando-a como 'excluir_treino'
     path('excluir/<int:pk>/', views.excluir_treino, name='excluir_treino'),
 ]
