9439d3e04c0c0d96a9383926692811b8907f9948
savioomio <115905335+savioomio@users.noreply.github.com>
2023-08-27 23:08:21 -0300

    .

--- lis_tre_Sávio/settings.py
+++ lis_tre_Sávio/settings.py
@@ -100,13 +100,13 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-#(para o sistema em produção)
 
+"""(para o sistema em produção)
 ALLOWED_HOSTS = ['sistema-treinos-de-savio.up.railway.app']
 CSRF_TRUSTED_ORIGINS = [
     "https://sistema-treinos-de-savio.up.railway.app",
 ]
-
+"""
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
